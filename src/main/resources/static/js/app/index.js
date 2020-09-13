a36da9a85cd51d4de1ca076d31d8dc5251e97f23
song <songgao12@naver.com>
2020-09-13 21:04:32 +0900

    test 0.0.3

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -16,13 +16,13 @@ var main={
             title:$('#title').val(),
             author:$('#author').val(),
             content:$('#content').val()
-        };var params = $("#testForm").serialize();
+        };
         $.ajax({
             type:'POST',
             url: '/api/v1/posts',
             dataType:'json',
             contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(params)
+            data: JSON.stringify(data)
         }).done(function (){
             alert('글이 등록되었습니다.');
             window.location.href='/';
